490dd97e25376c17e95dcd4395f95957bc512344
FloMenus <florent.mns@gmail.com>
2025-01-20 09:21:00 +0100

    feat: added articles

--- src/app/articles/[link]/layout.tsx
+++ src/app/articles/[link]/layout.tsx
@@ -3,7 +3,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Article - CSSéducteur",
   description: "",
-  authors: [{ name: 'Tom DEPUSSAY' }, { name: 'Florent MENUS' }, { name: 'Thomas LEJEUNE' }],
+  authors: [
+    { name: "Tom DEPUSSAY" },
+    { name: "Florent MENUS" },
+    { name: "Thomas LEJEUNE" },
+  ],
 };
 
 export default function ArticleLayout({
