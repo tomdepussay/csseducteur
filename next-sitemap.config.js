73ab88b687e5311e4edf155fc653ded00da2f929
Tom Depussay <depussay.tom1@gmail.com>
2025-01-23 11:37:04 +0100

    Changement sitemap pour les routes dynamique

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,31 @@
+const articles = require('./src/data/articles.tsx');
+const frameworks = require('./src/data/frameworks.tsx');
+
+async function getDynamicPaths() {
+  let paths = [];
+
+  frameworks.default.forEach(framework => {
+    paths.push({
+      loc: `/frameworks/${framework.link}`,
+    });
+  });
+  
+  articles.default.forEach(article => {
+    paths.push({
+      loc: `/articles/${article.link}`,
+    });
+  })
+
+  return paths;
+}
+
 module.exports = {
     siteUrl: 'https://www.csseducteur.com',
     generateRobotsTxt: true,
     changefreq: 'daily',
     priority: 0.7,
     sitemapSize: 5000,
+    additionalPaths: async (config) => {
+      return await getDynamicPaths();
+    } 
   };  
